8293c051776d2820a1001d2b8fe812d886b4f7a2
karen1au <kagemikaka@hotmail.com>
2019-03-19 22:12:44 +0000

    added function to render tweets from database, fixed background height

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,91 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
+
+$(document).ready(function() {
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+function renderTweets(tweets) {
+  for (let individual = 0; individual < tweets.length; individual++) {
+    $('#tweets-container').append(createTweetElement(tweets[individual]));
+  }
+
+}
+
+
+function createTweetElement(data) {
+  let $tweet = `
+    <article>
+      <header>
+        <img class="avatar" src=${escape(data.user.avatars.small)}>
+        <span class="user_name">${escape(data.user.name)}</span>
+        <span class="user_id">${escape(data.user.handle)}</span>
+      </header>
+      <div class="content">
+        <p>${escape(data.content.text)}</p>
+      </div>
+      <footer>
+        <span class="post_date">${data.created_at}</span>
+        <div class="icon">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
+    return $tweet;
+
+}
+renderTweets(data);
+
+});
